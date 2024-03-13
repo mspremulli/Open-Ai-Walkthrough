1516044bf817dff09f9905b4ee697e7be84fe9e1
Michael Spremulli <michaels.wpi@gmail.com>
2024-03-13 16:40:00 -0400

    Update assistant.tsx

--- open-ai-walkthrough/src/pages/assistant.tsx
+++ open-ai-walkthrough/src/pages/assistant.tsx
@@ -16,41 +16,16 @@ const Assistant: FC<Props> = ({assistantId, apiKey}) => {
 
   useEffect(() => {
     // const thread = await openai.beta.threads.create();
-    const createThread = async () => {
-      setThread(await openai.beta.threads.create());
-    }
-    if (thread) return
-    createThread();
 
   }, [])
 
   const updateMessages = async (intervalId: NodeJS.Timeout) => {
-    clearInterval(intervalId);
-    if (!thread) return;
-    const messages = await openai.beta.threads.messages.list(
-      thread.id
-    )
-    setMessageList(messages.data.reverse());
+    
 
   }
   
   const handleQuery = async () => {
-    if (!thread) return;
-    setQuery("");
-    try {
-      const intervalId = setInterval(async () => {
-        console.log('This will run every 3 seconds');
-        const res = await openai.beta.threads.runs.retrieve(thread.id, run.id);
-        if (res.status == "completed") {
-          updateMessages(intervalId);
-        }
-      }, 3000);
-
-
-    }
-    catch (error) {
-      console.error("An error occurred:", error);
-    }
+    
    
   };
 
@@ -63,7 +38,10 @@ const Assistant: FC<Props> = ({assistantId, apiKey}) => {
 
   return (
     <div className="container mx-auto p-4">
-      <form onSubmit={handleQuery} className="flex flex-col text-center w-full mb-6 max-w-4xl">
+      <form 
+        onSubmit={(e) => {e.preventDefault(); handleQuery()}} 
+        className="flex flex-col text-center w-full mb-6 max-w-4xl"
+      >
          <div className="mt-2 flex flex-row items-center">
             <input 
               style={{flex: 1}}
